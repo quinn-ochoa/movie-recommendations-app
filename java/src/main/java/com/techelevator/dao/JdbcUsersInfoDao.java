9e1decfa92ea4382c26e2ea65d3ced0c92a8254f
Larry Carter[D[D[D[D[D[DDevin Carter <larrydevincarter@gmail.com>
2024-04-09 11:47:55 -0500

    added ability to get user information by id and update user information as well

--- java/src/main/java/com/techelevator/dao/JdbcUsersInfoDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcUsersInfoDao.java
@@ -2,11 +2,15 @@ package com.techelevator.dao;
 
 import com.techelevator.dao.UsersInfoDao;
 import com.techelevator.exception.DaoException;
+import com.techelevator.model.User;
 import com.techelevator.model.UsersInfo;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class JdbcUsersInfoDao implements UsersInfoDao {
@@ -22,7 +26,7 @@ public class JdbcUsersInfoDao implements UsersInfoDao {
     //methods
     @Override
     public void addProfileInfo(UsersInfo usersInfo) {
-//TODO Working here
+
         String sql = "INSERT INTO users_info (user_id, email, full_name, birthday) VALUES (?,?,?,?);";
 
         try{
@@ -38,5 +42,60 @@ public class JdbcUsersInfoDao implements UsersInfoDao {
             throw new DaoException("Data integrity violation", e);
 
         }
+
     }
+
+    @Override
+    public void updateProfileInfo(UsersInfo usersInfo) {
+
+        String sql = "UPDATE users_info SET email = ?, full_name = ?, birthday = ? WHERE user_id = ?;";
+
+        try {
+
+            jdbcTemplate.update(sql, usersInfo.getEmail(), usersInfo.getFull_name(), usersInfo.getBirthday(), usersInfo.getUser_id());
+
+        } catch (CannotGetJdbcConnectionException e) {
+
+            throw new DaoException("Unable to connect to server or database", e);
+
+        } catch (DataIntegrityViolationException e) {
+
+            throw new DaoException("Data integrity violation", e);
+
+        }
+
+    }
+
+    @Override
+    public UsersInfo getUserInfo(int user_id) {
+
+        UsersInfo usersInfo = null;
+        String sql = "SELECT * FROM users_info WHERE user_id = ?";
+
+        try {
+
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, user_id);
+
+            if (results.next()) {
+
+                usersInfo = mapRowToUser(results);
+
+            }
+
+        } catch (CannotGetJdbcConnectionException e) {
+
+            throw new DaoException("Unable to connect to server or database", e);
+
+        } return usersInfo;
+    }
+
+    private UsersInfo mapRowToUser(SqlRowSet results) {
+        UsersInfo usersInfo = new UsersInfo();
+        usersInfo.setUser_id(results.getInt("user_id"));
+        usersInfo.setEmail(results.getString("email"));
+        usersInfo.setFull_name(results.getString("full_name"));
+        usersInfo.setBirthday(results.getTimestamp("birthday"));
+        return usersInfo;
+    }
+
 }
--- java/src/main/java/com/techelevator/dao/UsersInfoDao.java
+++ java/src/main/java/com/techelevator/dao/UsersInfoDao.java
@@ -5,4 +5,8 @@ import com.techelevator.model.UsersInfo;
 public interface UsersInfoDao {
 
     void addProfileInfo(UsersInfo usersInfo);
+
+    void updateProfileInfo(UsersInfo usersInfo);
+
+    UsersInfo getUserInfo(int user_id);
 }
