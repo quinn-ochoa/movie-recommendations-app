a9c0a5ba108fcf251e0ca52467a10c0e50e32b41
Quynh Ochoa <quynh.ochoa@gmail.com>
2024-04-16 09:55:38 -0700

    update password reset

--- vue/src/views/UpdatePasswordView.vue
+++ vue/src/views/UpdatePasswordView.vue
@@ -104,7 +104,7 @@ export default {
                 .then((response) => {
                     if (response.status == 200) {
                         // this.$store.commit("SET_AUTH_TOKEN", response.data.token);
-                        this.$store.commit("SET_USER", response.data.user);
+                        // this.$store.commit("SET_USER", response.data.user);
                         this.$router.push("/");
                     }
                 })
